cd6b6feeb46b1008a3a9076affc1766ec9c79f4b
shayaanakbar <shayaanakbar1106@gmail.com>
2019-06-25 21:14:34 -0500

    Testing Display Currently passing

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,17 @@
-// Test away!
+import React from 'react';
+import { render,  cleanup } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+import 'jest-dom/extend-expect';
+import Display from './Display';
+
+describe('<Display />', () => {
+  it('should display if gate is closed', () => {
+    const { getByText } = render(<Display closed={true}/>)
+    expect(getByText(/Closed/i)).toBeInTheDocument();
+  });
+
+  it('should display that the gate is opened if it is unlocked', () => {
+    const { getByText } = render(<Display closed={false}/>)
+    expect(getByText(/Open/i)).toBeInTheDocument();
+  });
+})
