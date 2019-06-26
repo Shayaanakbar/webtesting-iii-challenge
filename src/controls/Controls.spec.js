7b5d29982139741571881885a460e188a46ae1b7
shayaanakbar <shayaanakbar1106@gmail.com>
2019-06-25 20:44:45 -0500

    Working on controls.spec

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,18 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+import 'jest-dom/extend-expect';
+
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+  it('should provide buttons to toggle closed and locked states', () => {
+    const { getByText }  = render(<Controls locked={false} closed={false}/>)
+
+    const lockButton = getByText(/lock Gate/i)
+    const closeButton = getByText(/close/i)
+
+    expect(lockButton).toBeInTheDocument();
+    expect(closeButton).toBeInTheDocument();
+  });
+});
